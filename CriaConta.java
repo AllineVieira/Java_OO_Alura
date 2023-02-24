a870540cd8aa3c364914e6265b566d9c45f00e94
Alline Vieira <alline.gio@hotmail.com>
2023-02-24 17:47:07 -0300

    Adding more exercises

--- CriaConta.java
+++ CriaConta.java
@@ -4,10 +4,10 @@ public class CriaConta {
 	public static void main(String[] args) {
 		Conta primeiraConta = new Conta();
 		primeiraConta.saldo = 200;
-		System.out.println(primeiraConta.saldo);
+		//System.out.println(primeiraConta.saldo);
 		
 		primeiraConta.saldo += 100;
-		System.out.println(primeiraConta.saldo);
+		//System.out.println(primeiraConta.saldo);
 		
 		
 		Conta segundaConta = new Conta();
@@ -16,5 +16,21 @@ public class CriaConta {
 		System.out.println("Primeira conta tem " + primeiraConta.saldo);
 		System.out.println("Segunda conta tem " + segundaConta.saldo);
 		
+		System.out.println(primeiraConta.agencia);
+		System.out.println(primeiraConta.numero);
+		System.out.println(primeiraConta.titular);
+		
+		segundaConta.agencia = 146;
+		System.out.println("Agora a segunda conta está na ag "
+				+ segundaConta.agencia);
+		
+		if(primeiraConta == segundaConta) {
+			System.out.println("Mesma conta");
+		} else {
+			System.out.println("Contas diferentes");
+		}
+		
+		System.out.println(primeiraConta);
+		System.out.println(segundaConta);
 	}
 }
